63b144586d5b2e997cc540e6a7bc7b5168a28d92
eliaseriksson <eliaseriksson95@gmail.com>
2020-08-14 00:21:41 +0200

    added docs.
    merged Client.send_message into Client.upload as the method was only used there once and it is still short.
    
    fixed concurrency issues when multiple sources were to be processed simultaneously, the client now uses different sockets for each processing task.

--- Client/Codescord/client.py
+++ Client/Codescord/client.py
@@ -7,6 +7,12 @@ from .errors import *
 
 
 def setup_socket() -> socket.socket:
+    """
+    sets up a socket used by the client
+
+    blocking must be false since used in async context
+    :return: the clients socket used to connect to the processing server
+    """
     sock = socket.socket()
     sock.setblocking(False)
     return sock
@@ -20,39 +26,71 @@ class Source:
 
 class Client:
     def __init__(self, loop=None) -> None:
+        """
+        initialises the client object with event loop and max retries for connection errors.
+
+        :attr loop: the event loop
+        :attr retries: amount of times to retry the connection if it dies.
+
+        :param loop: the event loop
+        """
+
         print("initializing...")
-        self.socket = setup_socket()
         self.loop = loop if loop else asyncio.get_event_loop()
         self.retries = 3
         print("initialized.")
 
-    def close(self) -> None:
-        print("closing...")
-        self.socket.close()
-        self.socket = setup_socket()
-        print("closed.")
+    async def response_as_int(self, connection: socket.socket, length=utils.Protocol.buffer_size, endian="big", signed=False) -> int:
+        """
+        receive an integer from the server.
 
-    async def response_as_int(self, length=utils.Protocol.buffer_size, endian="big", signed=False) -> int:
-        print("SLEEPING")
-        integer = int.from_bytes((await self.loop.sock_recv(self.socket, length)), endian, signed=signed)
-        print(f"\tgot response as int ({integer}).")
+        most integers will be 1 byte big (status codes) but occasionally larger (reception size).
+        mostly a convenience method to automatically convert received bytes to int.
+        used for status codes and reception size (number of bytes).
+
+        :param length: buffer size.
+        :param endian: big or little endian int.
+        :param signed: singed or unsigned int
+        :param connection: connection to the processing server
+
+        :return: None
+        """
+        print("awaiting response as int...")
+        integer = int.from_bytes((await self.loop.sock_recv(connection, length)), endian, signed=signed)
+        print(f"got response as int ({integer}).")
         return integer
 
-    async def send_int_as_bytes(self, integer: int, length=utils.Protocol.buffer_size, endian="big", signed=False) -> None:
+    async def send_int_as_bytes(self, connection: socket.socket, integer: int, length=utils.Protocol.buffer_size, endian="big", signed=False) -> None:
         print(f"sending int ({integer}) as bytes...")
-        await self.loop.sock_sendall(self.socket, integer.to_bytes(length, endian, signed=signed))
+        await self.loop.sock_sendall(connection, integer.to_bytes(length, endian, signed=signed))
         print(f"sent int ({integer}) as bytes.")
 
-    async def assert_response_status(self, status=utils.Protocol.Status.success) -> None:
-        print(f"\tasserting response status ({status})...")
-        response = await self.response_as_int()
+    async def assert_response_status(self, connection: socket.socket, status=utils.Protocol.Status.success) -> None:
+        """
+        makes sure the server response is the the same the client expects
+
+        makes sure the server response as expected.
+        raises the correct error if this is not the case.
+
+        :param connection: connection to the server.
+        :param status: the expected status code.
+
+        :raises ConnectionError: if anything goes wrong with the connection (DCs etc).
+        :raises NotImplementedByServer: if the instruction is not implemented by the server.
+        :raises NotImplementedByClient: if the instruction is not implemented by the client.
+        :raises InternalServerError:if the server can communicate but something goes wrong on the server side.
+
+        :return: None
+        """
+        print(f"asserting response status ({status})...")
+        response = await self.response_as_int(connection)
         if response == status:
-            print(f"\tresponse passed assertion ({status}).")
+            print(f"response passed assertion ({status}).")
         elif response == utils.Protocol.Status.not_implemented:
-            print(f"\tresponse was `{response}` (not implemented) expected `{status}`.")
+            print(f"response was `{response}` (not implemented) expected `{status}`.")
             raise NotImplementedByServer()
         elif response == utils.Protocol.Status.internal_server_error:
-            print(f"\tresponse was `{response}` (internal server error) expected `{status}`.")
+            print(f"response was `{response}` (internal server error) expected `{status}`.")
             raise InternalServerError()
         elif response not in [getattr(utils.Protocol, attr) for attr in dir(utils.Protocol.Status)]:
             print(f"response `{response}` does not exist in Protocol.")
@@ -60,89 +98,168 @@ class Client:
         else:
             raise AssertionError(f"expected status: {status}, got: {response} instead.")
 
-    async def send_size(self, size: int, endian="big", signed=False) -> None:
-        print("sending size...")
-        # number of bytes required to store the size in an int
-        bites = ceil(size.bit_length() / 8)
-        await self.send_int_as_bytes(bites, endian=endian, signed=signed)
-        await self.assert_response_status(utils.Protocol.Status.success)
+    async def download(self, connection: socket.socket) -> bytes:
+        """
+        downloads some byte blob from the server.
 
-        await self.send_int_as_bytes(size, bites)
-        await self.assert_response_status(utils.Protocol.Status.success)
-        print("sent size.")
+        used to download whats captured in stdout from the processed source.
 
-    async def download(self) -> bytes:
+        :raises ConnectionError: if anything goes wrong with the connection (DCs etc)
+
+        :param connection: connection to the processing server.
+        :return: None
+        """
+
         print("downloading...")
         # number of bytes required to store the size of the blob in an int
-        bites = await self.response_as_int()
-        await self.send_int_as_bytes(utils.Protocol.Status.success)
+        bites = await self.response_as_int(connection)
+        await self.send_int_as_bytes(connection, utils.Protocol.Status.success)
 
         # size of the blob in number of bytes
-        size = await self.response_as_int(bites)
-        await self.send_int_as_bytes(utils.Protocol.Status.success)
+        size = await self.response_as_int(connection, bites)
+        await self.send_int_as_bytes(connection, utils.Protocol.Status.success)
 
         # initialising blob and downloading from socket, blob will be `size` bytes
         blob = b""
         for _ in range(int(size / utils.Protocol.max_buffer)):
-            blob += await self.loop.sock_recv(self.socket, utils.Protocol.max_buffer)
-        blob += await self.loop.sock_recv(self.socket, (size % utils.Protocol.max_buffer))
+            blob += await self.loop.sock_recv(connection, utils.Protocol.max_buffer)
+        blob += await self.loop.sock_recv(connection, (size % utils.Protocol.max_buffer))
         print("downloaded.")
         return blob
 
-    async def upload(self, payload: bytes) -> None:
-        print("uploading...")
-        await self.send_size(len(payload))
+    async def upload(self, connection: socket.socket, payload: bytes) -> None:
+        """
+        uploads some byte blob to the processing server
 
-        await self.loop.sock_sendall(self.socket, payload)
-        await self.assert_response_status(utils.Protocol.Status.success)
+        used to upload files to the processing server.
+        sends a 1 byte message about how many bytes that the integer representing the size of the upload is,
+        then sends the integer representing the size of the upload.
+        the byte blob is the uploaded for the processing server to download.
+
+        :param connection: connection to the processing server.
+        :param payload: the byte blob of data to be sent to the recipient.
+
+        :raises ConnectionError: if anything goes wrong with the connection (DCs etc).
+        :raises InternalServerError:if the server can communicate but something goes wrong on the server side.
+
+        :return: None
+        """
+        print("uploading...")
+        # size of the upload
+        size = len(payload)
+
+        # number of bytes required to receive the entire integer representing the size of the upload
+        bites = ceil(size.bit_length() / 8)
+
+        await self.send_int_as_bytes(connection, bites)
+        await self.assert_response_status(connection, utils.Protocol.Status.success)
+
+        await self.send_int_as_bytes(connection, size, bites)
+        await self.assert_response_status(connection, utils.Protocol.Status.success)
+
+        await self.loop.sock_sendall(connection, payload)
+        await self.assert_response_status(connection, utils.Protocol.Status.success)
         print("uploaded.")
 
-    async def authenticate(self) -> None:
+    async def authenticate(self, connection: socket.socket,) -> None:
+        """
+        authenticates the protocol that is used by the client and server
+
+        by authenticating the protocol the client sends the protocol to the server
+        and the server will compare and verify it. if the protocols match the process can go on
+
+        if server denies verification NotImplementedByServer is raised to indicate the protocols are not the same
+        if something else goes wrong on server side InternalServerError is raised
+
+        :param connection: the connection to the processing server
+
+        :raises NotImplementedByServer: if the instruction is not implemented by the server.
+        :raises ConnectionError: if any sort of connection error occurs.
+        :raises InternalServerError: if the server can communicate but something goes wrong on the other side.
+
+        :return: None
+        """
         print("authenticating...")
-        await self.send_int_as_bytes(utils.Protocol.Status.authenticate)
-        await self.assert_response_status(utils.Protocol.Status.success, )
+        await self.send_int_as_bytes(connection, utils.Protocol.Status.authenticate)
+        await self.assert_response_status(connection)
 
         payload = utils.Protocol.get_protocol().encode("utf-8")
-        await self.upload(payload)
+        await self.upload(connection, payload)
         print("authenticated.")
 
-    async def handle_source(self, source: Source) -> None:
-        print("handling the source...")
+    async def handle_source(self, connection: socket.socket, source: Source) -> None:
+        """
+        handles the sending of the source file
 
-        await self.send_int_as_bytes(utils.Protocol.Status.file)
-        await self.assert_response_status(utils.Protocol.Status.success)
+        attempts to send the source file to the processing server
+
+        if anything goes wrong InternalServerError is raised
+
+        :raises: InternalServerError,
+
+        :param connection:
+        :param source: source object with language and source code
+        :return: None
+        """
+        print("handling the source...")
+        await self.send_int_as_bytes(connection, utils.Protocol.Status.file)
+        await self.assert_response_status(connection, utils.Protocol.Status.success)
 
         payload = source.language.encode("utf-8")
-        await self.upload(payload)
+        await self.upload(connection, payload)
 
         payload = source.code.encode("utf-8")
-        await self.upload(payload)
+        await self.upload(connection, payload)
         print("source handled.")
 
-    async def handle_stdout(self) -> str:
+    async def handle_stdout(self, connection: socket.socket) -> str:
+        """
+        handles the receiving of the stdout from the server when processing the source file.
+
+        awaits the server to respond with a message if message is a text message the
+        source files produced stdout will be downloaded from the processing server.
+
+        if message is not a text message NotImplementedByClient is raised.
+
+        :raises: NotImplementedByClient
+        :return:
+        """
         print("handling stdout...")
-        response = await self.response_as_int()
+        response = await self.response_as_int(connection)
         if response == utils.Protocol.Status.text:
-            await self.send_int_as_bytes(utils.Protocol.Status.success)
-            blob = await self.download()
-            await self.send_int_as_bytes(utils.Protocol.Status.success)
+            await self.send_int_as_bytes(connection, utils.Protocol.Status.success)
+            blob = await self.download(connection)
+            await self.send_int_as_bytes(connection, utils.Protocol.Status.success)
             print("stdout handled.")
             return blob.decode("utf-8")
+        await self.send_int_as_bytes(connection, utils.Protocol.Status.not_implemented)
         raise NotImplementedByClient(f"handle_stdout cant handle {response}")
 
-    async def handle_connection(self, source: Source) -> str:
+    async def handle_connection(self, connection: socket.socket, source: Source) -> str:
+        """
+        the main procedure of the processing of the source
+
+        makes sure the client and server authenticates before proceeding.
+        sends the source to the processing server.
+        waits for the processing server to send results back, times out after 30 seconds.
+        downloads the results and returns them.
+
+        :param connection: the connection to the processing server
+        :param source:
+        :return:
+        """
         print("handling the connection...")
         try:
-            await self.authenticate()
-            await self.handle_source(source)
+            await self.authenticate(connection)
+            await self.handle_source(connection, source)
 
-            await self.send_int_as_bytes(utils.Protocol.Status.awaiting)
-            stdout = await self.handle_stdout()
-            await self.assert_response_status(utils.Protocol.Status.awaiting)
+            await self.send_int_as_bytes(connection, utils.Protocol.Status.awaiting)
+            stdout = await self.handle_stdout(connection)
+            await self.assert_response_status(connection, utils.Protocol.Status.awaiting)
 
             print("client starting to send close")
-            await self.send_int_as_bytes(utils.Protocol.Status.close)
-            await self.assert_response_status(utils.Protocol.Status.success)
+            await self.send_int_as_bytes(connection, utils.Protocol.Status.close)
+            await self.assert_response_status(connection, utils.Protocol.Status.success)
 
             print("connection handled.")
             return stdout
@@ -163,9 +280,11 @@ class Client:
         :param init: if initial call, makes sure it doesnt spam self.close() if all connection tries fails
         :return:
         """
+
+        connection = setup_socket()
         try:
-            await self.loop.sock_connect(self.socket, ("localhost", 6969))
-            stdout = await self.handle_connection(source)
+            await self.loop.sock_connect(connection, ("localhost", 6969))
+            stdout = await self.handle_connection(connection, source)
             return stdout if stdout else "There was an error processing the source."
         except KeyboardInterrupt:
             pass
@@ -179,5 +298,4 @@ class Client:
         finally:
             # this runs 3 times in a row if full connectionError is happening
             if init:
-                self.close()
-                print("disconnected.")
+                connection.close()
--- Client/main.py
+++ Client/main.py
@@ -4,27 +4,46 @@ import DiscordClient
 import os
 
 
-def run() -> None:
-    py_source = Codescord.Source(
+async def runner():
+    py1 = Codescord.Source(
         language="py",
-        code="print('Hello World!')"
+        code="from time import sleep\nsleep(3)\nprint('py1')"
     )
-    cpp_source = Codescord.Source(
-        language="cpp",
-        code="""#include <iostream>
-        int main(){
-            std::cout << "Hello world!" << std::endl;
-            return 0;
-        }"""
+
+    py2 = Codescord.Source(
+        language="py",
+        code="from time import sleep\nsleep(2)\nprint('py2')"
     )
+
+    py3 = Codescord.Source(
+        language="py",
+        code="from time import sleep\nsleep(1)\nprint('py3')"
+    )
+
+    scripts = [
+        py1, py2, py3
+    ]
 
     loop = asyncio.get_event_loop()
     client = Codescord.client.Client(loop)
 
-    result = loop.run_until_complete(
-        client.process(py_source)
-    )
-    print(result)
+    coroutines = [
+        asyncio.create_task(
+            client.process(script)
+        )
+        for script in scripts
+    ]
+
+    results = [
+        await future for future in coroutines
+    ]
+
+    print(results)
+
+
+def run() -> None:
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(runner())
 
 
 def run_as_discord():
