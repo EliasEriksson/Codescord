5ba9374306269d5da23fbf28a5bdc31903c5c3ac
eliaseriksson <eliaseriksson95@gmail.com>
2020-08-21 18:44:26 +0200

    work on filesystem changed detection. if there are file system changes outside of "normal" the container the applications runs in should be nuked.

--- testing.py
+++ testing.py
@@ -1,38 +1,57 @@
-from typing import *
+import asyncio
+from aionotify import Flags, Watcher
+from pathlib import Path
 
 
-def autocast_to_annotations(func):
-    def wrapper(*args, **kwargs):
-        # inspecting args
-        args = list(args)
-        m = dict(list(zip(
-                func.__code__.co_varnames, range(len(func.__code__.co_varnames)))
-            )[:func.__code__.co_argcount]
-        )
-        for arg, index in m.items():
-            if arg in func.__annotations__:
-                t = func.__annotations__[arg]
-                value = args[index]
-                correct_type = isinstance(value, t)
-                if not correct_type:
-                    args[index] = t(value)
-        # inspecting kwargs
-        for kwarg, value in kwargs.items():
-            if kwarg in func.__annotations__:
-                t = func.__annotations__[kwarg]
-                correct_type = isinstance(value, t)
-                if not correct_type:
-                    kwargs[kwarg] = t(value)
 
-        return func(*args, **kwargs)
-    return wrapper
+class FileWatcher(Watcher):
+    def __init__(self, root_path: Path, *ignored_directories: Path) -> None:
+        super(FileWatcher, self).__init__()
+        self.watch_descriptors_needed = 0
+        self.setup_watcher(root_path, *ignored_directories)
+        print(self.watch_descriptors_needed)
+
+        with (path := Path("/proc/sys/fs/inotify")).joinpath("max_user_watches").open("r") as f:
+            current_max_amount_of_wd = int(f.read(1))
+
+        # without proper permissions code bellow will error with permissions denied. if True
+        # try running code in subprocess instead and the user will be asked to provide password instead
+        # if too many watch descriptors are needed increase the max cap
+        if self.watch_descriptors_needed > current_max_amount_of_wd:
+            with path.joinpath("max_user_watches").open("w") as f:
+                f.write(str(self.watch_descriptors_needed + 1000))
+            with path.joinpath("max_queued_events").open("w") as f:
+                f.write(str((self.watch_descriptors_needed + 1000) // 4))
+            with path.joinpath("max_user_instances").open("w") as f:
+                f.write(str((self.watch_descriptors_needed + 1000) // 515))
 
 
-@autocast_to_annotations
-def foo(x: bool, a: str, b: str, ) -> str:
-    print(x)
-    return a + b
+
+    def setup_watcher(self, directory: Path, *ignored_directories: Path):
+        try:
+            flags = Flags.MODIFY | Flags.CREATE | Flags.DELETE | Flags.MOVED_FROM | Flags.ATTRIB | Flags.DONT_FOLLOW
+            self.watch(str(directory), flags=flags)
+            self.watch_descriptors_needed += 1
+            for path in directory.iterdir():
+                if path.is_dir() and not path.is_symlink():
+                    if path not in ignored_directories:
+                        self.setup_watcher(path, *ignored_directories)
+        except PermissionError:
+            pass
 
 
-result = foo(True, 1, 2, asd="123")
-print(result)
+async def main(loop):
+    watcher = FileWatcher(Path("/home/elias-eriksson"), Path("/home/elias-eriksson/Dev/Pyscord/ignore"))
+    print("set up")
+    await watcher.setup(loop)
+    print("ready")
+    while event := await watcher.get_event():
+        print(event)
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(main(loop))
+    except KeyboardInterrupt:
+        pass
