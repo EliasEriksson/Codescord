75c82ba6e07b0e2a2cb8e6e38c375a1f6e6e3c34
EliasEriksson <EliasEriksson95@gmail.com>
2020-09-15 20:08:07 +0200

    Sources's internal sys_args no longer defaults to None

--- Codescord/Common/source.py
+++ Codescord/Common/source.py
@@ -2,4 +2,4 @@ class Source:
     def __init__(self, language: str, code: str, sys_args: str = None) -> None:
         self.language = language
         self.code = code
-        self.sys_args = sys_args
+        self.sys_args = sys_args if sys_args else ""
